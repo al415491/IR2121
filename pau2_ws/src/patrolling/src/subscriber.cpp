b0fae1dc6b31017c4f412bee1ceba8dd00f1b1f2
al415491 <al415491@uji.es>
2023-11-30 16:11:33 +0100

    Patrolling v2.1

--- pau2_ws/src/patrolling/src/subscriber.cpp
+++ pau2_ws/src/patrolling/src/subscriber.cpp
@@ -11,15 +11,11 @@ int main(int argc, char * argv[])
 {
   rclcpp::init(argc, argv);
   auto node = rclcpp::Node::make_shared("subscriber");
-  auto subscription = node->create_subscription<std_msgs::msg::String>("topic", 10, topic_callback);
+  auto subscription = node->create_subscription<std_msgs::msg::String>("/odom", 10, topic_callback);
   rclcpp::spin(node);
   rclcpp::shutdown();
   return 0;
 }
 
 
-/*
-el subscriber espera y no hace nada si no llegan mensajes
-l5 muestra el mensaje
-l15 quedarse inactivo espreando msgs
-*/
+
